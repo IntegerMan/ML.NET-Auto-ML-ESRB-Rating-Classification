774cccef1c402c22036dcf6ccb0796542d4ae6ea
Matt Eland <matt.eland@gmail.com>
2021-11-28 17:38:44 -0500

    Work on training

---
+++ MattEland.AI.Samples.MLNet/Movie.cs
@@ -0,0 +1,36 @@
+﻿public class Movie
+{
+    public int Id { get; set; }
+    public bool IsAction { get; set; }
+    public bool IsAdventure { get; set; }
+    public bool IsAnimation { get; set; }
+    public bool IsComedy { get; set; }
+    public bool IsCrime { get; set; }
+    public bool IsDocumentary { get; set; }
+    public bool IsDrama { get; set; }
+    public bool IsFamily { get; set; }
+    public bool IsFantasy { get; set; }
+    public bool IsForeign { get; set; }
+    public bool IsHorror { get; set; }
+    public bool IsMusic { get; set; }
+    public bool IsMystery { get; set; }
+    public bool IsRomance { get; set; }
+    public bool IsScienceFiction { get; set; }
+    public bool IsTVMovie { get; set; }
+    public bool IsThriller { get; set; }
+    public bool IsWar { get; set; }
+    public bool IsWestern { get; set; }
+    public bool IsAdult { get; set; }
+    public string Overview { get; set; }
+    public int ReleaseMonth { get; set; }
+    public int ReleaseQuarter { get; set; }
+    public int ReleaseYear { get; set; }
+    public int RuntimeInMinutes { get; set; }
+    public string Tagline { get; set; }
+    public string Title { get; set; }
+
+    public bool IsChristmasMovie { get; set; }
+    public string Keywords { get; set; }
+
+
+}
--- MattEland.AI.Samples.MLNet/Program.cs
+++ MattEland.AI.Samples.MLNet/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.ML;
+using Microsoft.ML.AutoML;
+
 Console.WriteLine("Hello, World!");
+
+MLContext mlContext = new MLContext();
+IDataView trainDataView = mlContext.Data.LoadFromTextFile<Movie>("traintest.csv", hasHeader: true);
+
+var experimentSettings = new BinaryExperimentSettings();
+
+experimentSettings.MaxExperimentTimeInSeconds = 3600;
+experimentSettings.OptimizingMetric = BinaryClassificationMetric.F1Score;
+
+var experiment = mlContext.Auto().CreateBinaryClassificationExperiment(experimentSettings);
+var result = experiment.Execute(trainDataView, labelColumnName: nameof(Movie.IsChristmasMovie));
+
+Console.WriteLine("Best F1 Score: " + result.BestRun.ValidationMetrics.F1Score);
